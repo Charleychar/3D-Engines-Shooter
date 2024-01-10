a0cae64afab64bd1df14db4a50dce635ec71fab6
Charleychar <Charleychar@outlook.com>
2024-01-10 16:02:31 +0000

    Fixed falling through floor bug

---
+++ 3D Engines Shooter/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    //variables
+    [SerializeField] public float speed = 5f;
+    [SerializeField] public float sensitivity = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3D Engines Shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 20f;
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+      
+        
+    }
+
+    
+
+    
+}
