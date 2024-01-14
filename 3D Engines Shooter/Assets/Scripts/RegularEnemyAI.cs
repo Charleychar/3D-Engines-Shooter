4d323a6134a88baa766bacdca5c168adaad18bca
Charleychar <Charleychar@outlook.com>
2024-01-14 18:05:56 +0000

    Working on enemy animation and animation events
    currently on the regular enemy type, with 'idle', 'chasing' and 'attacking' animations states

--- 3D Engines Shooter/Assets/Scripts/RegularEnemyAI.cs
+++ 3D Engines Shooter/Assets/Scripts/RegularEnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,25 +8,88 @@ public class RegularEnemyAI : MonoBehaviour
 {
     //parameters
     [SerializeField] float aggroRange = 5f;
-    float distanceToTarget = Mathf.Infinity;
+    float distanceToPlayer = Mathf.Infinity;
     [SerializeField] float turnSpeed = 3f;
 
     //references
     [SerializeField] Transform playerTarget;
     NavMeshAgent nMA;
     Animator anim;
+
+    //states
+    bool Aggro = false;
     
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        nMA = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
+    //public static bool Raycast(Vector3 gameObject, Vector3 forwards, float aggroRange);
+    
+    
+    
+    
+    
+    
+    
     // Update is called once per frame
     void Update()
     {
+        if (playerTarget != null)
+        {
+            distanceToPlayer = Vector3.Distance(playerTarget.position, transform.position);
+        }
+
+        if (Aggro)
+        {
+            EngagePlayer();
+        }
+
+        else if (distanceToPlayer <= aggroRange)
+        {
+            Aggro = true;
+        }
+    }
+
+    private void EngagePlayer()
+    {
+        FacePlayer();
+
+        if (distanceToPlayer > nMA.stoppingDistance)
+        {
+            ChasePlayer();
+        }
+
+        else if (distanceToPlayer <= nMA.stoppingDistance)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        anim.SetBool("attackingPlayer", true);
+    }
+
+    private void ChasePlayer()
+    {
+        anim.SetTrigger("playerSpotted");
+        anim.SetBool("attackingPlayer", false);
+        nMA.SetDestination(playerTarget.position);
+
+    }
+
+    private void FacePlayer()
+    {
         
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
     }
 }
